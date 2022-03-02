7caa25668643e53a7ab3e20d32391eb1dfe75888
mhwalid7 <mhwalid7@gmail.Com>
2022-03-02 10:43:55 +0100

    make REpository for entity

---
+++ src/Repository/User/UserRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repository\User;
+
+use App\Repository\AbstractRepository;
+
+class UsertRepository extends AbstractRepository{
+
+    public const TABLE = 'users';
+
+}
+
+
+?>
